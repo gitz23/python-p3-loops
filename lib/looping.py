6fb70d9391d7b7472c3ccbef705e967e80595f7d
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-08-18 00:18:57 +0300

    complete assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,41 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!") 
+# #test
+# happy_new_year()
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    sq_int_list = []
+    for int in int_list:
+       sq_num = int ** 2
+       sq_int_list.append(sq_num)
+    return(print(sq_int_list))
+# #test
+# square_integers([3,4,5,6])
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Fizz")
+        else:
+            print(num)
+# #test
+# fizzbuzz()
+
+def reversed_string(string):
+    reverse_string = ""
+    for letter in string:
+        reverse_string = letter + reverse_string
+    return reverse_string
+# #test
+# print(reversed_string("input string"))
